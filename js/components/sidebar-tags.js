3af8cd6ff52054bab3edc3647db0548ff61aee2a
Mahdi Luqman Abdulrahman <mahdy.00523775@gmail.com>
2026-08-13 18:41:15 +0300

    refactor: assemble sidebar taxonomy methods before owner

--- js/components/sidebar-tags.js
+++ js/components/sidebar-tags.js
@@ -1,4 +1,4 @@
-window.SidebarTagMethods = {
+window.SidebarTagMethods = Object.assign({
   renderTags() { return window.SidebarTaxonomyUiCore.render(this, 'tag'); },
   createTagTreeNode(tag, depth = 0) { return window.SidebarTaxonomyUiCore.createNode(this, 'tag', tag, depth); },
   openTagModal(tagId = null, parentId = null, trigger = null) { return window.SidebarTaxonomyUiCore.open(this, 'tag', tagId, parentId, trigger); },
@@ -6,4 +6,9 @@ window.SidebarTagMethods = {
   saveTag() { return window.SidebarTaxonomyUiCore.save(this, 'tag'); },
   deleteTag(tagId) { return window.SidebarTaxonomyUiCore.remove(this, 'tag', tagId); },
   closeTagModal() { return window.SidebarTaxonomyUiCore.close(this, 'tag'); }
-};
+},
+  window.SidebarTaxonomyDragMethods || {},
+  window.SidebarTaxonomyDragHierarchyMethods || {},
+  window.SidebarTaxonomyDragTouchMethods || {},
+  window.SidebarTaxonomyDragCommitMethods || {}
+);
