f596a2a730f4b0261848cb379466ea6c73bfd6c9
Mahdi Luqman Abdulrahman <mahdy.00523775@gmail.com>
2026-08-13 18:46:48 +0300

    refactor: load core application modules

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,1 +1,10 @@
 // Native module bootstrap entry.
+import './task-model.js';
+import './state.js';
+import './task-relations.js';
+import './task-order.js';
+import './taxonomy-order.js';
+import './task-filter.js';
+import './repeat/repeat-engine.js';
+import './theme.js';
+import './components/modal-focus.js';
