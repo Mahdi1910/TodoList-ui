4e61fdac7811a38ccf03a3e6818d37a9c287570a
Mahdi Luqman Abdulrahman <mahdy.00523775@gmail.com>
2026-08-13 18:47:20 +0300

    refactor: load database modules statically

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -8,3 +8,9 @@ import './task-filter.js';
 import './repeat/repeat-engine.js';
 import './theme.js';
 import './components/modal-focus.js';
+
+import './storage/db-schema.js';
+import './storage/db.js';
+import './storage/repositories.js';
+import './storage/mappers.js';
+import './storage/persistence.js';
