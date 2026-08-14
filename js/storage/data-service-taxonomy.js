5c0ad48f6d55794500db47b6780513bf23918b9f
Mahdi Luqman Abdulrahman <mahdy.00523775@gmail.com>
2026-08-14 08:39:43 +0300

    ID20 part 1: keep taxonomy service taxonomy-focused

--- js/storage/data-service-taxonomy.js
+++ js/storage/data-service-taxonomy.js
@@ -111,36 +111,5 @@ Object.assign(window.AppDataService, {
       Object.assign(entity, updated);
       return entity;
     });
-  },
-
-  saveReminderDefinition(custom) {
-    return this.enqueue(async () => {
-      const definition = window.TodoStorageMappers.customReminderToDefinition(custom);
-      if (!definition) throw new Error('Invalid custom reminder.');
-      const S = window.TodoDbSchema.STORES;
-      await window.TodoDb.withTransaction(S.REMINDER_DEFINITIONS, 'readwrite', tx =>
-        window.TodoRepositories.put(tx, S.REMINDER_DEFINITIONS, definition)
-      );
-      return definition;
-    });
-  },
-
-  deleteReminderDefinition(reminderId) {
-    return this.enqueue(async () => {
-      const S = window.TodoDbSchema.STORES;
-      const deleted = await window.TodoDb.withTransaction([S.REMINDER_DEFINITIONS, S.TASK_REMINDERS], 'readwrite', async tx => {
-        const definition = await window.TodoRepositories.get(tx, S.REMINDER_DEFINITIONS, reminderId);
-        if (!definition || definition.isBuiltin) return false;
-        await window.TodoRepositories.deleteByIndex(tx, S.TASK_REMINDERS, 'by_reminder_id', reminderId);
-        await window.TodoRepositories.remove(tx, S.REMINDER_DEFINITIONS, reminderId);
-        return true;
-      });
-      if (!deleted) return false;
-      window.AppState.tasks.forEach(task => {
-        task.reminders = (task.reminders || []).filter(id => id !== reminderId);
-        if (!task.reminders.length) task.reminders = ['none'];
-      });
-      return true;
-    });
   }
 });
